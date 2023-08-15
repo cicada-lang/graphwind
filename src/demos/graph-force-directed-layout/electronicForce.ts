fd2dbca6d419db5ed5867fade76110d617aa69b5
Xie Yuheng <xyheme@gmail.com>
2023-08-16 01:27:52 +0800

    up

--- src/demos/graph-force-directed-layout/electronicForce.ts
+++ src/demos/graph-force-directed-layout/electronicForce.ts
@@ -2,7 +2,7 @@ export function electronicForce(
   first: [number, number],
   second: [number, number],
 ): [number, number] {
-  const C = 1000 * 1000 
+  const C = 1000 * 1000
 
   const deltaX = second[0] - first[0]
   const deltaY = second[1] - first[1]
@@ -19,9 +19,6 @@ export function electronicForce(
   if (isNaN(forceX) || isNaN(forceY)) {
     return [0, 0]
   }
-
-  console.log(first, second)
-  console.log(forceX, forceY)
 
   return [forceX, forceY]
 }
